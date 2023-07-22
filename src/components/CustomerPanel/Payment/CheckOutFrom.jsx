d135184fe8630a8cfa7a75693856675525f8ab42
byomkesh2580 <habibrahman2580@gmail.com>
2023-07-23 03:38:21 +0600

    beforebkash

--- src/components/CustomerPanel/Payment/CheckOutFrom.jsx
+++ src/components/CustomerPanel/Payment/CheckOutFrom.jsx
@@ -21,14 +21,10 @@ const CheckOutFrom = ({ closeModal, select, selectInfo }) => {
   // });
 
   const [userId, setUserId] = useState(user?.email);
-  console.log(userId);
   const [promoCode, setPromoCode] = useState("");
-  console.log(promoCode);
   const [productName, setProductName] = useState(select?.name);
-  console.log(productName);
   const [message, setMessage] = useState("");
   const [finalPrice, setFinalPrice] = useState(null);
-  console.log(finalPrice);
 
   const handleApplyPromoCode = (event) => {
     event.preventDefault();
@@ -53,7 +49,6 @@ const CheckOutFrom = ({ closeModal, select, selectInfo }) => {
       });
   };
 
-
   // useEffect(() => {
   //   if (discount > 0) {
   //     axiosSecure
@@ -64,31 +59,35 @@ const CheckOutFrom = ({ closeModal, select, selectInfo }) => {
   //   }
   // }, [select, axiosSecure]);
 
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   const form = event.target;
-  //   const productId = select?.selectItemId;
-  //   const name = user.displayName;
-  //   const email = user.email;
-  //   const itemName = select?.name;
-  //   const price = select?.price;
-  //   const newPrice = parseFloat(price);
-  //   const useremail = form.useremail.value;
-  //   const username = form.username.value;
-  //   const checkItems = {
-  //     productId,
-  //     newPrice,
-  //     itemName,
-  //     useremail,
-  //     username,
-  //     name,
-  //     email,
-  //   };
-  //   console.log(checkItems);
-  // };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const productId = select?.selectItemId;
+    const name = user.displayName;
+    const email = user.email;
+    const promo = promoCode;
+    const itemName = select?.name;
+    const oldprice = select?.price;
+    const price = parseFloat(oldprice);
+    const discountPrice = finalPrice;
+    const useremail = form.useremail.value;
+    const username = form.username.value;
+    const checkItems = {
+      productId,
+      price,
+      itemName,
+      useremail,
+      username,
+      discountPrice,
+      promo,
+      name,
+      email,
+    };
+    console.log(checkItems);
+  };
   return (
     <div>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div className="flex mt-2 justify-around">
           <div>
             <div className="mt-2">
@@ -98,7 +97,7 @@ const CheckOutFrom = ({ closeModal, select, selectInfo }) => {
             </div>
             <div className="mt-2">
               <p className="text-md text-gray-900">
-                Price: {select?.price} (BDT)
+                Main Price: {select?.price} (BDT)
               </p>
             </div>
             <div className="space-y-1 text-sm py-3">
@@ -128,20 +127,33 @@ const CheckOutFrom = ({ closeModal, select, selectInfo }) => {
             </div>
             <div className="space-y-1 text-sm py-3">
               <label htmlFor="promo" className="block text-gray-900">
-                promo
+                Promo Code (if Any)
               </label>
-              <input
-                className="w-full px-4 py-3 text-gray-900 border border-rose-300 focus:outline-rose-500 rounded-md "
-                name="promo"
-                id="promo"
-                type="promo"
-                value={promoCode}
-                onChange={(e) => setPromoCode(e.target.value)}
-                placeholder="Promo Code"
-              />
-              <button onClick={handleApplyPromoCode}>Apply Promo Code</button>
-              {message && <p>{message}</p>}
-              {finalPrice !== null && <p>Final Price: {finalPrice}</p>}
+              <div className="flex items-center gap-5">
+                <input
+                  className="w-full px-4 py-3 text-gray-900 border border-rose-300 focus:outline-rose-500 rounded-md "
+                  name="promo"
+                  id="promo"
+                  type="promo"
+                  value={promoCode}
+                  onChange={(e) => setPromoCode(e.target.value)}
+                  placeholder="Promo Code"
+                />
+                <button
+                  onClick={handleApplyPromoCode}
+                  className="btn btn-sm bg-green-600"
+                >
+                  Apply Promo
+                </button>
+              </div>
+              {message && (
+                <p className="text-md text-red-600 text-center">{message}</p>
+              )}
+              {finalPrice !== null && (
+                <p className="text-xl text-blue-700 text-center">
+                  Final Price: {finalPrice}৳
+                </p>
+              )}
             </div>
             <hr className="mt-8 " />
             <div className="flex mt-2 justify-around">
@@ -156,7 +168,7 @@ const CheckOutFrom = ({ closeModal, select, selectInfo }) => {
                 type="submit"
                 className="inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2"
               >
-                Pay $ {finalPrice > 0 ? finalPrice : select?.price}
+                Pay {finalPrice > 0 ? finalPrice : select?.price}৳
               </button>
             </div>
           </div>
